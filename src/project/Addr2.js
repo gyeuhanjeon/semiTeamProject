02c072199945ceb714efe412059a4c9e5ec7614e
GONGRYUNG <alsgud413@naver.com>
2022-10-24 20:19:46 +0900

    민형

--- src/project/Addr2.js
+++ src/project/Addr2.js
@@ -3,9 +3,9 @@ import hangjungdong from "./hangjungdong";
 
 export default function App2() {
   const [val1, setVal1] = useState("");
-  const [val2, setVal2] = useState("");
-  const [setVal3] = useState("");
-  const { sido, sigugun, dong } = hangjungdong;
+  const [setVal2] = useState("");
+
+  const { sido, sigugun} = hangjungdong;
   return (
     <div>
       <select onChange={(e) => setVal1(e.target.value)}>
@@ -22,16 +22,6 @@ export default function App2() {
           .filter((el) => el.sido === val1)
           .map((el) => (
             <option key={el.sigugun} value={el.sigugun}>
-              {el.codeNm}
-            </option>
-          ))}
-      </select>
-      <select onChange={(e) => setVal3(e.target.value)}>
-        <option value="">선택</option>
-        {dong
-          .filter((el) => el.sido === val1 && el.sigugun === val2)
-          .map((el) => (
-            <option key={el.dong} value={el.dong}>
               {el.codeNm}
             </option>
           ))}
