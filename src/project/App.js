b4ca65072dac1a93febd12a210652061598f7a1e
gyeu han jeon <gyeurok@naver.com>
2022-10-24 17:20:09 +0900

    첫커밋

---
+++ src/project/App.js
@@ -0,0 +1,20 @@
+// import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './Main';
+import Login from './Login';
+import SignUp from './SignUp';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/project/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+
+    return(
+        <div>
+            <div> 엠비티아이셔</div>
+            <button>시작하기</button>
+        </div>
+    );
+}
+export default Main;
